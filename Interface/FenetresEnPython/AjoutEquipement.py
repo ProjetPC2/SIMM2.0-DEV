da30b36a879917bf07c405356e530094d9278711
sabrinamoisan <smoisan18@gmail.com>
2016-07-25 11:54:56 -0400

    Revert "ajout du champ code asset"
    This reverts commit 27ab0ce8c71e7f1a341f47a8569c3eecef2844b4.

--- Interface/FenetresEnPython/AjoutEquipement.py
+++ Interface/FenetresEnPython/AjoutEquipement.py
@@ -52,7 +52,6 @@ class AjoutEquipement(Ui_AjoutEquipement):
         self.listeWidgets.append(self.dateEditDateDaquisition)
         self.listeWidgets.append(self.dateEditDateDuDernierEntretien)
         self.listeWidgets.append(self.comboBoxProvenance)
-        self.listeWidgets.append(self.lineEditCodeASSET)
         self.listeWidgets.append(self.groupeBoutonEtatService)
         self.listeWidgets.append(self.groupeBoutonEtatConservation)
         self.listeWidgets.append(self.textEditCommentaires)
@@ -121,7 +120,6 @@ class AjoutEquipement(Ui_AjoutEquipement):
         self.listeLabel.append(self.labelDateDAquisition)
         self.listeLabel.append(self.labelDateDernierEntretien)
         self.listeLabel.append(self.labelProvenance)
-        self.listeLabel.append(self.labelCodeASSET)
         self.listeLabel.append(self.labelEtatDeService)
         self.listeLabel.append(self.labelEtatDeConservation)
 
