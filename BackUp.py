20e039d2a3cd88d569849b72bc440e99a6ac5645
MaoAlex <mao.alexandre@gmail.com>
2016-08-22 23:04:33 +0200

    ajout fonctionnalité
    Ajout de la copie des fichiers de la BDD sous Windows

--- BackUp.py
+++ BackUp.py
@@ -11,15 +11,22 @@ def backUp():
     Par défaut ce dossier se trouvera créer sur le bureau"""
     directory = QFileDialog.getExistingDirectory(None, 'Create Dir', os.path.expanduser("~/Desktop/"),
                                                 QFileDialog.ShowDirsOnly)
+
     print(directory)
-    systemOS = sys.platform
     directory = directory + "/SIMMBackUp"
+    systemOS = sys.platform
     print(os.path.expanduser(directory))
     if systemOS == "linux":
-        os.system("mkdir" + directory)
-        os.system("cp DataBase_Equipement.yaml Database_BDT.yaml fichier_conf.yaml fichier_stats.yaml fichier_pieces.yaml" + os.path.expanduser(directory))
+        os.system("mkdir " + directory)
+        os.system("cp DataBase_Equipement.yaml DataBase_BDT.yaml fichier_conf.yaml fichier_stats.yaml fichier_pieces.yaml   " + os.path.expanduser(directory))
     elif systemOS == "win32":
         print("Ordinateur sous Windows")
+        print(os.path.expanduser(directory))
+        path = "\"" + os.path.expanduser(directory) + "\""
+        #path = os.path.expanduser(directory)
+        os.system("mkdir " + path)
+        os.system("robocopy " + "./ " + path + " DataBase_Equipement.yaml DataBase_BDT.yaml fichier_conf.yaml fichier_stats.yaml fichier_pieces.yaml ")
+
     else:
         print("Autre OS")
 
