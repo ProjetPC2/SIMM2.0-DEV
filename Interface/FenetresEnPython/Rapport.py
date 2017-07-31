8440a447abb869c4c5d25637e1913d7019313914
NicoBernier <nicolas.bernier@polymtl.ca>
2017-07-31 10:18:29 -0400

    Changement du nom de l'hôpital dans les rapports pdf
    Self.nomHopital ne fonctionnait pas bien

--- Interface/FenetresEnPython/Rapport.py
+++ Interface/FenetresEnPython/Rapport.py
@@ -58,8 +58,8 @@ class Rapport():
         espace_soulignement = 10
         facteurDivision = 10
         espacement = 130
-        technicien = "Techniciens"
-        canvas.drawString(self.PAGE_WIDTH / facteurDivision, 3 * self.PAGE_HEIGHT / 4, 'Hôpital ' + self.nomHopital)
+        technicien = "Techniciens de l'Hôpital Universitaire Justinien"
+        canvas.drawString(self.PAGE_WIDTH / facteurDivision, 3 * self.PAGE_HEIGHT / 4, 'Hôpital Universitaire Justinien'
         canvas.line(self.PAGE_WIDTH / facteurDivision, 3 * self.PAGE_HEIGHT / 4 - espace_soulignement,
                     self.PAGE_WIDTH / facteurDivision + 230, 3 * self.PAGE_HEIGHT / 4 - espace_soulignement)
         # canvas.setLineWidth(1.2)
@@ -68,7 +68,7 @@ class Rapport():
         canvas.drawString(500, 750, "12/12/2010")
         canvas.line(480, 747, 580, 747)
         canvas.drawString(6 * self.PAGE_WIDTH / facteurDivision, 3 * self.PAGE_HEIGHT / 4,
-                          "Délivré par l'atelier de génie biomédical de " + self.nomHopital)
+                          "Délivré par l'atelier de génie biomédical de l'HUJ"
         canvas.line(6 * self.PAGE_WIDTH / facteurDivision, 3 * self.PAGE_HEIGHT / 4 - espace_soulignement,
                     6 * self.PAGE_WIDTH / facteurDivision + 295, 3 * self.PAGE_HEIGHT / 4 - espace_soulignement)
         date = time.strftime("%d/%m/%Y")
